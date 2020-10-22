d317c38dc92947f13747a621930e4169f85b56a4
Valdi Kamenarov <v.kamenarov@gmail.com>
2020-10-22 10:55:57 +0300

    Add vim-go and utilsnips modules

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 
 # Plugins
 git submodule add https://github.com/preservim/nerdtree.git plugins/start/nerdtree
+git submodule add https://github.com/fatih/vim-go.git plugins/start/vim-go
+git submodule add https://github.com/SirVer/ultisnips.git plugins/start/utilsnips
 
 # Syntax
 
